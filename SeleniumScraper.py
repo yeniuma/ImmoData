cf0bbcaa3a42b56032fa50fb330ba40657d1b9ee
yeniuma <manczaladri@gmail.com>
2023-12-30 16:23:40 +0100

    ♻️ code changed so it exports a csv for every page instead of only at the end

--- SeleniumScraper.py
+++ SeleniumScraper.py
@@ -2,7 +2,11 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
+#from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support import expected_conditions as EC
+from random import randrange
 import time
 import pandas as pd
 
@@ -21,119 +25,126 @@ def driver_startup(url):
     service_path = Service(executable_path='F:/ChromeDriver/chromedriver.exe')
     options = Options()
     #options.add_argument('--headless=new')
+    #options.add_argument('--disable-notifications')
+    options.add_argument("--mute-audio")
     driver = webdriver.Chrome(service=service_path, options=options)
     driver.get(url)
-    time.sleep(20)
+    driver.implicitly_wait(40)
+    ##accept_cookies = WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="uc-center-container"]/div[2]/div/div/div/button[2]')))
+    #driver.execute_script("arguments[0].click()", accept_cookies)
+    #driver.find_element(by=By.XPATH, value='/html/body/div[6]//div/div/div[2]/div/div[2]/div/div[2]/div/div/div/button[2]').click()
+    #time.sleep(5)
     return driver
 
 def check_find_elements(selector_value, driver):
-    temp_list = []
     if len(driver.find_elements(by=By.CSS_SELECTOR,value=selector_value)) == 0:
-        temp_list = [textHelper('NA')]
+        temp = textHelper('NA')
     else:
-        temp_list = driver.find_elements(by=By.CSS_SELECTOR,value=selector_value)[:1]
-    return temp_list
+        temp = driver.find_elements(by=By.CSS_SELECTOR,value=selector_value)[0]
+    return temp
 
 def scrape_immoscout_rentals(city):
-    rentals_df = pd.DataFrame()
-
-    property_type = []
-    address = []
-    region_and_country = []
-    street = []
-    apartment_size = []
-    floor = []
-    able_to_move_in = []
-    rooms = []
-    bathrooms = []
-    cold_price = []
-    cold_price_per_sqm = []
-    warm_price = []
-    labels = []
-    pets_allowed = []
-    year_built = []
-    heating = []
-    heating_price = []
-    parking = []
-    deposit = []
-    status = []
-    desc = []
-    property_url = []
-    website = []
-
     driver = driver_startup(page)
-
-    for j in range(5):
-        driver.find_element(by=By.ID, value = f'listing-{j}-slide-0').click()
-        time.sleep(10)
-        driver.back()
-        time.sleep(10)
-    #driver.find_element(by=By.ID,value='listing-0-slide-0').click()
 
     i = 1
     while True :
-        time.sleep(2)
         print("{} {} {} {}".format('scraping page', i,'from', city))
-        property_type_list = check_find_elements('.is24qa-typ.grid-item.three-fifths', driver)
-        address_list = check_find_elements('.address-block', driver)
-        region_and_country_list = check_find_elements('.zip-region-and-country', driver)
-        street_list = check_find_elements('.block.font-nowrap.print-hide', driver)
-        apartment_size_list = check_find_elements('.is24qa-wohnflaeche-ca.grid-item.three-fifths', driver)
-        floor_list = check_find_elements('.is24qa-etage.grid-item.three-fifths', driver)
-        able_to_move_in_list = check_find_elements('.is24qa-bezugsfrei-ab.grid-item.three-fifths', driver)
-        rooms_list = check_find_elements('.is24qa-zimmer.grid-item.three-fifths', driver)
-        bathrooms_list = check_find_elements('.is24qa-badezimmer.grid-item.three-fifths', driver)
-        cold_price_list = check_find_elements('.is24-preis-value', driver)
-        cold_price_per_sqm_list = check_find_elements('.is24qa-kaltmiete-main-label.is24-label.font-s', driver)
-        warm_price_list = check_find_elements('.is24qa-geschaetzte-warmmiete-main.is24-value.font-semibold', driver)
-        labels_list = check_find_elements('.criteriagroup.boolean-listing.padding-top-l', driver)
-        pets_allowed_list = check_find_elements('.is24qa-haustiere.grid-item.three-fifths', driver)
-        year_built_list = check_find_elements('.is24qa-baujahr.grid-item.three-fifths', driver)
-        heating_list = check_find_elements('.is24qa-heizungsart.grid-item.three-fifths', driver)
-        heating_price_list = check_find_elements('.is24qa-heizkosten.grid-item.three-fifths', driver)
-        parking_list = check_find_elements('.is24qa-garage-stellplatz.grid-item.three-fifths', driver)
-        deposit_list = check_find_elements('.is24qa-kaution-o-genossenschaftsanteile', driver)
-        status_list = check_find_elements('.is24qa-objektzustand.grid-item.three-fifths', driver)
-        desc_list = check_find_elements('.is24qa-lage.text-content.short-text', driver)
-        property_url_list = driver.current_url
-        website_list = website
-        for property_type_item, address_item, region_and_country_item, street_item, apartment_size_item, floor_item, able_to_move_in_item, rooms_item, bathrooms_item, cold_price_item, cold_price_per_sqm_item, warm_price_item, labels_item, pets_allowed_item, year_built_item, heating_item, heating_price_item, parking_item, deposit_item, status_item, desc_item in zip(property_type_list, region_and_country_list, address_list, street_list,apartment_size_list,floor_list,able_to_move_in_list,rooms_list,bathrooms_list,cold_price_list,cold_price_per_sqm_list,warm_price_list,labels_list,pets_allowed_list,year_built_list,heating_list,heating_price_list,parking_list,deposit_list,status_list,desc_list):
-            property_type.append(property_type_item.text)
-            address.append(address_item.text)
-            region_and_country.append(region_and_country_item.text)
-            street.append(street_item.text)
-            apartment_size.append(apartment_size_item.text)
-            floor.append(floor_item.text)
-            able_to_move_in.append(able_to_move_in_item.text)
-            rooms.append(rooms_item.text)
-            bathrooms.append(bathrooms_item.text)
-            cold_price.append(cold_price_item.text)
-            cold_price_per_sqm.append(cold_price_per_sqm_item.text)
-            warm_price.append(warm_price_item.text)
-            labels.append(labels_item.text)
-            pets_allowed.append(pets_allowed_item.text)
-            year_built.append(year_built_item.text)
-            heating.append(heating_item.text)
-            heating_price.append(heating_price_item.text)
-            parking.append(parking_item.text)
-            deposit.append(deposit_item.text)
-            status.append(status_item.text)
-            desc.append(desc_item.text)
-            property_url.append(property_url_list)
-            website.append(website_list)
-        break
 
-    temp_df = pd.DataFrame({'Property_type': property_type, 'Street':street, 'Region_and_country': region_and_country, 'Apt_size': apartment_size, 'Floor':floor,
-                            'Move_in':able_to_move_in, 'Nr_of_rooms': rooms, 'Nr_of_bathrooms':bathrooms, 'Cold_price':cold_price, 'Cold_price_per_sqm': cold_price_per_sqm,
-                            'Warm_price':warm_price, 'Labels': labels, 'Pets_allowed' : pets_allowed, 'Year_built': year_built, 'Heating': heating, 'Heating_price': heating_price, 'Parking': parking,
-                            'Deposit': deposit, 'Status': status, 'Desc': desc, 'URL': property_url})
-    print('Total numbers of properties available in ' + city + ' is ' + str(temp_df.shape[0]))
+        property_type_list = []
+        address_list = []
+        region_and_country_list = []
+        street_list = []
+        apartment_size_list = []
+        floor_list = []
+        able_to_move_in_list = []
+        rooms_list = []
+        bathrooms_list = []
+        cold_price_list = []
+        cold_price_per_sqm_list = []
+        warm_price_list = []
+        labels_list = []
+        pets_allowed_list = []
+        year_built_list = []
+        heating_list = []
+        heating_price_list = []
+        parking_list = []
+        deposit_list = []
+        status_list = []
+        desc_list = []
+        property_url_list = []
 
-    rentals_df = pd.concat([rentals_df, temp_df], ignore_index=True)
+        for j in range(20):
+            driver.find_element(by=By.ID, value = f'listing-{j}-slide-0').click()
 
-    return rentals_df
+            property_type = check_find_elements('.is24qa-typ.grid-item.three-fifths', driver).text
+            address = check_find_elements('.address-block', driver).text
+            region_and_country = check_find_elements('.zip-region-and-country', driver).text
+            street = check_find_elements('.block.font-nowrap.print-hide', driver).text
+            apartment_size= check_find_elements('.is24qa-wohnflaeche-ca.grid-item.three-fifths', driver).text
+            floor = check_find_elements('.is24qa-etage.grid-item.three-fifths', driver).text
+            able_to_move_in = check_find_elements('.is24qa-bezugsfrei-ab.grid-item.three-fifths', driver).text
+            rooms = check_find_elements('.is24qa-zimmer.grid-item.three-fifths', driver).text
+            bathrooms = check_find_elements('.is24qa-badezimmer.grid-item.three-fifths', driver).text
+            cold_price = check_find_elements('.is24-preis-value', driver).text
+            cold_price_per_sqm = check_find_elements('.is24qa-kaltmiete-main-label.is24-label.font-s', driver).text
+            warm_price = check_find_elements('.is24qa-geschaetzte-warmmiete-main.is24-value.font-semibold', driver).text
+            labels = check_find_elements('.criteriagroup.boolean-listing.padding-top-l', driver).text
+            pets_allowed = check_find_elements('.is24qa-haustiere.grid-item.three-fifths', driver).text
+            year_built= check_find_elements('.is24qa-baujahr.grid-item.three-fifths', driver).text
+            heating = check_find_elements('.is24qa-heizungsart.grid-item.three-fifths', driver).text
+            heating_price = check_find_elements('.is24qa-heizkosten.grid-item.three-fifths', driver).text
+            parking = check_find_elements('.is24qa-garage-stellplatz.grid-item.three-fifths', driver).text
+            deposit = check_find_elements('.is24qa-kaution-o-genossenschaftsanteile', driver).text
+            status = check_find_elements('.is24qa-objektzustand.grid-item.three-fifths', driver).text
+            desc = check_find_elements('.is24qa-lage.text-content.short-text', driver).text
+            property_url = driver.current_url
 
-test_df = scrape_immoscout_rentals('Vienna')
+            property_type_list.append(property_type)
+            address_list.append(address)
+            region_and_country_list.append(region_and_country)
+            street_list.append(street)
+            apartment_size_list.append(apartment_size)
+            floor_list.append(floor)
+            able_to_move_in_list.append(able_to_move_in)
+            rooms_list.append(rooms)
+            bathrooms_list.append(bathrooms)
+            cold_price_list.append(cold_price)
+            cold_price_per_sqm_list.append(cold_price_per_sqm)
+            warm_price_list.append(warm_price)
+            labels_list.append(labels)
+            pets_allowed_list.append(pets_allowed)
+            year_built_list.append(year_built)
+            heating_list.append(heating)
+            heating_price_list.append(heating_price)
+            parking_list.append(parking)
+            deposit_list.append(deposit)
+            status_list.append(status)
+            desc_list.append(desc)
+            property_url_list.append(property_url)
 
-with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
-    print(test_df)
+            driver.back()
+
+        temp_df = pd.DataFrame({'Property_type': property_type_list, 'Street':street_list, 'Region_and_country': region_and_country_list, 'Apt_size': apartment_size_list, 'Floor':floor_list,
+                                'Move_in':able_to_move_in_list, 'Nr_of_rooms': rooms_list, 'Nr_of_bathrooms':bathrooms_list, 'Cold_price':cold_price_list, 'Cold_price_per_sqm': cold_price_per_sqm_list,
+                                'Warm_price':warm_price_list, 'Labels': labels_list, 'Pets_allowed' : pets_allowed_list, 'Year_built': year_built_list, 'Heating': heating_list, 'Heating_price': heating_price_list, 'Parking': parking_list,
+                                'Deposit': deposit_list, 'Status': status_list, 'Desc': desc_list, 'URL': property_url_list})
+        temp_df.to_csv(f'{city}rentals_{i}_page.csv')
+
+        last_button_found = False
+        try:
+            driver.find_element(by = By.CSS_SELECTOR, value='.p-items.p-next.vertical-center-container.disabled')
+            last_button_found = True
+        except NoSuchElementException:
+            driver.find_element(by= By.CSS_SELECTOR, value='.p-items.p-next.vertical-center-container').click()
+
+        if last_button_found:
+            break
+
+        #ideiglenes
+        if i > 2: 
+            break
+
+        i += 1
+        time.sleep(randrange(5,10))
+
+scrape_immoscout_rentals('Vienna')
